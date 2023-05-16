1fed72db5151f0105edf10eb72cfd3392c6e2ec2
ggken <86958845+muskf@users.noreply.github.com>
2023-05-16 21:40:51 +0800

    Add files via upload

---
+++ api/api.php
@@ -0,0 +1,23 @@
+<?php
+// 获取公告内容
+$announcement = $_POST['announcement'];
+
+// 如果公告内容非空，则写入到文件中
+if(!empty($announcement)) {
+	$file = fopen('announcement.txt', 'w');
+	fwrite($file, $announcement);
+	fclose($file);
+	echo '公告发布成功！';
+	exit;
+}
+
+// 如果公告内容为空，则读取文件中的最新公告内容并返回
+if(file_exists('announcement.txt')) {
+	$file = fopen('announcement.txt', 'r');
+	$announcement = fread($file, filesize('announcement.txt'));
+	fclose($file);
+	echo $announcement;
+} else {
+	echo '暂无公告';
+}
+?>
