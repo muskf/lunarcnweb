a3b28044ebeb0d70f5b4a5c44f1de7d15badae21
CubeWhy <cubewhy@cubewhy.eu.org>
2023-06-22 07:05:25 +0800

    update links

---
+++ api/repo_version.php
@@ -0,0 +1,43 @@
+<?php
+
+header('Content-Type: text/plain; charset=utf-8');
+
+$owner = 'CubeWhy';
+
+$repo = 'LunarClient-CN';
+
+$url = "https://api.github.com/repos/{$owner}/{$repo}/releases/latest";
+
+$ch = curl_init($url);
+
+curl_setopt($ch, CURLOPT_HTTPHEADER, ['Accept: application/vnd.github.v3+json']);
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Liux; Android 8.1.0; OPPO R11st Build/OPM1.171019.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/62.0.3202.84 Mobile Safari/537.36');
+
+$response = curl_exec($ch);
+
+$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+curl_close($ch);
+
+if ($httpcode === 200) {
+
+  $data = json_decode($response, true);
+
+  $version = htmlspecialchars($data['tag_name'], ENT_QUOTES, 'UTF-8');
+
+  echo $version;
+
+} else {
+
+  http_response_code($httpcode);
+
+  error_log("Failed to get latest release version for {$owner}/{$repo}");
+
+  echo "v2.15.1-next-gen-b2";
+
+}
+
+?>
