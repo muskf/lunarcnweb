e4ac0a9abdaf6c94c4305990262c54e6319843e5
ggken <86958845+muskf@users.noreply.github.com>
2023-08-08 14:45:09 +0800

    Update text.js

--- src/js/text.js
+++ src/js/text.js
@@ -9,7 +9,7 @@ for (let i = 0; i < navbarItem.length; i++) {
 }
 
 var typed = new Typed(".multiple-text", {
-    strings: ['加入JavaAgents', "更多的帧数", "启动器汉化", "自定义JDK", "完全开源", "自定义启动参数"],
+    strings: ['支持添加JavaAgents', "启动器汉化", "自定义JDK", "完全开源", "自定义启动参数", "支持添加LunarCN Mod", "完全免费", "用爱发电", "离线登录"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
